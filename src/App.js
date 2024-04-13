03695d13a4799c51fdd0e1498fe002e46313680b
tatiana.ryzhova <tryzhova93@gmail.com>
2024-04-13 13:33:49 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/answer1" element={<AnswerPage1 />} />
+            <Route path="/answer2" element={<AnswerPage2 />} />
+        </Routes>
+    );
+}
+
+function HomePage() {
+    let navigate = useNavigate();
+    return (
+        <div style={{ textAlign: 'center', marginTop: '50px' }}>
+            <h1>What is your favorite color?</h1>
+            <button onClick={() => navigate('/answer1')} style={{ margin: '10px', padding: '10px' }}>Blue</button>
+            <button onClick={() => navigate('/answer2')} style={{ margin: '10px', padding: '10px' }}>Red</button>
+        </div>
+    );
+}
+
+function AnswerPage1() {
+    return <h1 style={{ textAlign: 'center', marginTop: '50px' }}>You chose Blue!</h1>;
+}
+
+function AnswerPage2() {
+    return <h1 style={{ textAlign: 'center', marginTop: '50px' }}>You chose Red!</h1>;
 }
 
 export default App;
